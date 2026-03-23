ff763648f08bad6ad23acfa3f5129830dda791b5
yumauda <firststep-yumauda@gmail.com>
2026-03-23 11:45:55 +0900

    Refactor front page to use common link template and add company page structure. Introduce new styles for common MV and history sections, and update Sass imports accordingly. Remove obsolete reserve file.

--- front-page.php
+++ front-page.php
@@ -156,39 +156,6 @@
       </div>
     </div>
   </section>
-  <section class="p-common-link">
-    <ul class="p-common-link__list">
-      <li class="p-common-link__item">
-        <a class="p-common-link__link" href="<?php echo esc_url(home_url('/shop/')); ?>">
-          <img decoding="async" loading="lazy" class="p-common-link__bg" src="<?php echo esc_url(get_template_directory_uri()); ?>/images/top/common-link/common-link-shop.png" alt="" width="4096" height="2736">
-          <span class="p-common-link__overlay" aria-hidden="true"></span>
-          <span class="p-common-link__body">
-            <span class="p-common-link__en">Shop</span>
-            <span class="p-common-link__ja">店舗一覧</span>
-          </span>
-        </a>
-      </li>
-      <li class="p-common-link__item">
-        <a class="p-common-link__link" href="<?php echo esc_url(home_url('/recruit/')); ?>">
-          <img decoding="async" loading="lazy" class="p-common-link__bg" src="<?php echo esc_url(get_template_directory_uri()); ?>/images/top/common-link/common-link-recruit.png" alt="" width="4096" height="2736">
-          <span class="p-common-link__overlay" aria-hidden="true"></span>
-          <span class="p-common-link__body">
-            <span class="p-common-link__en">Recruit</span>
-            <span class="p-common-link__ja">採用情報</span>
-          </span>
-        </a>
-      </li>
-      <li class="p-common-link__item">
-        <a class="p-common-link__link" href="<?php echo esc_url(home_url('/contact/')); ?>">
-          <img decoding="async" loading="lazy" class="p-common-link__bg" src="<?php echo esc_url(get_template_directory_uri()); ?>/images/top/common-link/common-link-contact-464dba.png" alt="" width="664" height="539">
-          <span class="p-common-link__overlay" aria-hidden="true"></span>
-          <span class="p-common-link__body">
-            <span class="p-common-link__en">Contact</span>
-            <span class="p-common-link__ja">お問い合わせ</span>
-          </span>
-        </a>
-      </li>
-    </ul>
-  </section>
+  <?php get_template_part('includes/common-link'); ?>
 </main>
 <?php get_footer(); ?>
